400a126e400d2c07aa0441ada9a4125d5a561e43
Nicole Chan <nictscnotes@gmail.com>
2026-06-01 07:06:35 +1000

    Add explanations to file how to use it

--- calculator/conftest.py
+++ calculator/conftest.py
@@ -2,5 +2,10 @@ import os
 import django
 from django.conf import settings
 
+""" This file is a utility file to allow my unit tests to talk to Django or the database"""
+
+## Tells Django to use the settings from calculator/settings.py which contains my database config, installed apps, etc
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calculator.settings')
+
+## Initialize Django with the above settings
 django.setup()
